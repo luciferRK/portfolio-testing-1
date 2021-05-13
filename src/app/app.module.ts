2512251f650749960117e237129010867574e105
Likhith RK <likhithr.cs17@sahyadri.edu.in>
2021-05-13 22:50:32 +0530

    Put Loader in seperate Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { HttpClientModule } from '@angular/common/http';
+import { PreLoaderComponent } from './pre-loader/pre-loader.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PreLoaderComponent
   ],
   imports: [
     BrowserModule,
